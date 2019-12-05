1b2d11b56a28c3e37f8303e22bd7ccf2eefc94cf
mrbenjoo <benjamin.sejdic1@gmail.com>
2019-12-05 16:51:37 +0100

    Please enter the commit message for your changes. Lines starting

--- app/src/main/java/com/benji/weatherswe/dayweather/servicelocator/DayWeatherServiceLocator.kt
+++ app/src/main/java/com/benji/weatherswe/dayweather/servicelocator/DayWeatherServiceLocator.kt
@@ -1,32 +1,31 @@
 package com.benji.weatherswe.dayweather.servicelocator
 
-import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.benji.data.datasource.UrlManager
-import com.benji.data.datasource.remote.GeocodingAPI
-import com.benji.data.datasource.remote.GeocodingRemoteDataSource
 import com.benji.data.datasource.remote.WeatherAPI
 import com.benji.data.datasource.remote.WeatherRemoteDataSource
-import com.benji.data.repository.GeocodingRepository
 import com.benji.data.repository.WeatherRepository
 import com.benji.weatherswe.BaseViewModelFactory
 import com.benji.weatherswe.dayweather.DayWeatherViewModel
 import com.benji.weatherswe.locationweather.servicelocator.LocationWeatherServiceLocator
 import com.benji.weatherswe.utils.DispatcherProvider
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.io.File
 
 object DayWeatherServiceLocator {
     private var weatherRepository: WeatherRepository? = null
 
 
-    private fun provideWeatherRepository(): WeatherRepository {
+    private fun provideWeatherRepository(fragment: Fragment): WeatherRepository {
         var weatherRepositoryTemp =
             weatherRepository
         if (weatherRepository == null) {
             weatherRepository = WeatherRepository(
-                provideWeatherAPI()
+                provideWeatherAPI(fragment.context!!.cacheDir)
             )
             weatherRepositoryTemp =
                 weatherRepository
@@ -34,17 +33,18 @@ object DayWeatherServiceLocator {
         return weatherRepositoryTemp!!
     }
 
-    private fun provideWeatherAPI(): WeatherRemoteDataSource =
+    private fun provideWeatherAPI(cache: File): WeatherRemoteDataSource =
         WeatherRemoteDataSource(
-            provideRetrofit().create(
+            provideRetrofit(cache).create(
                 WeatherAPI::class.java
             )
         )
 
-    private fun provideRetrofit(): Retrofit =
+    private fun provideRetrofit(cache: File): Retrofit =
         Retrofit.Builder()
             .baseUrl(UrlManager.API_SMHI_FORECAST_BASE_URL)
             .addConverterFactory(MoshiConverterFactory.create())
+            .client(provideOkHttpClient(cache))
             .build()
 
     fun provideWeatherViewModel(fragment: Fragment): DayWeatherViewModel {
@@ -53,13 +53,30 @@ object DayWeatherServiceLocator {
             BaseViewModelFactory {
                 DayWeatherViewModel(
                     DispatcherProvider,
-                    provideWeatherRepository(),
+                    provideWeatherRepository(fragment),
                     LocationWeatherServiceLocator.provideGeocodingRepository()
                 )
             })
             .get(DayWeatherViewModel::class.java)
     }
 
+    private fun provideCache(cachePath: File): Cache? {
+        var cache: Cache? = null
+
+        try {
+            val cacheSize = 10 * 1024 * 1024 // 10 MB
+            cache = Cache(cachePath, cacheSize.toLong())
+        } catch (exception: Exception) {
+
+        }
+        return cache
+    }
+
+    private fun provideOkHttpClient(cache: File): OkHttpClient {
+        return OkHttpClient.Builder()
+            .cache(provideCache(cache))
+            .build()
+    }
 
 
 }
